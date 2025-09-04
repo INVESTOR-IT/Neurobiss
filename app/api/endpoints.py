99d9e7e65f83338f1b611b878cf4770d70a73391
Svyatoslav Shareko <modek@mail.ru>
2025-09-04 09:54:34 +0300

    Добавил отправку данных в телебот

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,10 +1,18 @@
 from fastapi import APIRouter, Request
+from loguru import logger
+from dotenv import load_dotenv
+
+import aiogram
+import os
 
 from app.servise.servise import parse_amocrm_webhook_data
 from app.ai.ai import text_processing_using_openai
 
+load_dotenv()
 
 router = APIRouter()
+BOT = aiogram.Bot(token=os.getenv('TELEGRAM_BOT_TOKEN'))
+CHAT_ID = os.getenv('CHAT_ID')
 
 
 @router.get('/health')
@@ -16,5 +24,6 @@ async def health():
 async def amocrm_webhook(request: Request):
     form_data = await request.form()
     result = await parse_amocrm_webhook_data(form_data)
-    print(result)
-    result_openai = await text_processing_using_openai(result)
+    # result_openai = await text_processing_using_openai(result)
+    await BOT.send_message(chat_id=CHAT_ID, text=result)  # text=result_openai
+    logger.info(f'Данные отправлены пользователю: {CHAT_ID}')
